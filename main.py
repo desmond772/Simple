65a693b21e8b1389b4a84cd29280b15a1f997975
desmond772 <ngwad896@gmail.com>
2025-08-30 04:48:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ load_dotenv()
 POCKET_OPTION_SSID = os.getenv("POCKET_OPTION_SSID")
 WEBSOCKET_URL = os.getenv("WEBSOCKET_URL")
 ORIGIN = os.getenv("ORIGIN")
-USER_ID = os.getenv("USER_ID")
 
 # Define functions for handling messages and authentication
 async def receive_messages(websocket):
@@ -25,35 +24,28 @@ async def receive_messages(websocket):
 
 async def send_authentication(websocket):
     try:
-        auth_payload = {"session": POCKET_OPTION_SSID, "user_id": USER_ID}
-        await websocket.send(json.dumps(auth_payload))
+        auth_payload = f'42["auth",{{"session":"{POCKET_OPTION_SSID}","isDemo":0}}]'
+        await websocket.send(auth_payload)
         print("Authentication message sent.")
     except Exception as e:
         print(f"Error sending authentication: {e}")
 
 # Main function
 async def main():
-    if not WEBSOCKET_URL or not POCKET_OPTION_SSID or not USER_ID:
+    if not WEBSOCKET_URL or not POCKET_OPTION_SSID:
         print("Error: Missing environment variables. Check your .env file.")
         return
 
     headers = {
         "Origin": ORIGIN,
-        "Cookie": f"ssid={POCKET_OPTION_SSID}",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/533.36",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Cache-Control": "no-cache",
-        "Pragma": "no-cache",
-        "Connection": "Upgrade",
-        "Upgrade": "websocket",
-        "Sec-WebSocket-Version": "13",
     }
 
     connection_timeout = 10
     try:
         async with websockets.connect(
-            WEBSOCKET_URL, additional_headers=headers, open_timeout=connection_timeout
+            WEBSOCKET_URL,
+            additional_headers=headers,
+            open_timeout=connection_timeout,
         ) as websocket:
             print("WebSocket connection established.")
             auth_task = asyncio.create_task(send_authentication(websocket))
